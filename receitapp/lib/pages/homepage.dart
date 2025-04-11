92046202e8781d3624538881e0d68b184af6586e
Elisson Rocha <elissonrochaa@gmail.com>
2025-04-11 09:34:03 -0300

    update 04-04

--- receitapp/lib/pages/homepage.dart
+++ receitapp/lib/pages/homepage.dart
@@ -6,7 +6,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: Text('ReceitApp'),
@@ -19,6 +18,28 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           LinhaWidget('Café da Manhã'),
+          Stack(children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              height: 136,
+              width: 136,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY5LHQFV-DU5SKonCG9ayBDER86b6JhGcTSA&s'),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              width: 120,
+              height: 88,
+              margin: EdgeInsets.only(left: 8, top: 8),
+            ),
+          ]),
           LinhaWidget('Almoço'),
           LinhaWidget('Jantar'),
         ],
